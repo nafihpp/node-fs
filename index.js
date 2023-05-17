3c4b97c5e9186dcf5fd1490a9f259709a0f21767
nafihpp <nafihpp@gmail.com>
2023-05-17 23:32:21 +0530

    server+nodeFs

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 const fs = require("fs");
 const { add, sub, divide, multiply } = require("./utils");
+const { createServer } = require("http");
 
+//simple Node Js Server
+const server = createServer((req, res) => {
+    switch (req.url) {
+        case "/": {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("Hello world");
+            break;
+        }
+        case "/products": {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end("<h1>Page Not Found 404 Error</h1>");
+            break;
+        }
+        case "/userList": {
+            fs.readFile("myContext.txt", (error, data) => {
+                if (data) {
+                    res.writeHead(200, { "Content-Type": "text/json" });
+                    res.end(data);
+                } else {
+                    res.writeHead(404, { "Content-Type": "text/html" });
+                    res.end(`<h1>Page Not Found 404 ${error}</h1>`);
+                }
+            });
+        }
+    }
+});
+server.listen(5000, () => {
+    console.log("listening to port");
+});
+
+//module exports function
 console.log(add(10, 20));
 console.log(sub(10, 20));
 console.log(multiply(10, 20));
 console.log(divide(10, 20));
 
-//create a file if not exist
-fs.writeFile("myContext.txt", "this is nodeJS task", (error) => {
-    if (error) {
+//read
+fs.writeFileSync("mycontentNew.txt", "welcome to the website", (err) => {
+    if (err) {
         console.log(error);
-    } else {
-        //append
-        fs.appendFile("myContext.txt", "Appending this here", (error) => {
-            if (error) {
-                console.log(error);
-            }
-        });
-        //readFile
-        fs.readFile("myContext.txt", "utf8", (error, data) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(data);
-            }
-        });
     }
 });
-
+//append
+fs.appendFileSync(
+    "mycontentNew.txt",
+    "\n this is a appending meessage",
+    (error) => {
+        console.log(error);
+    }
+);
 //create a folder
 // fs.mkdir(path.join(__dirname, "test"), (error) => {
 //     if (error) {
